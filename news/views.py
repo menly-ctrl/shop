094e990c2ace6b0def281494f6dac0dec97c2928
menly-ctrl <g.ravkoov@mail.ru>
2021-05-16 16:46:51 +0500

    buyer

---
+++ news/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .models import Items
+
+def index(request):
+    items_list = Items.objects.all()
+    data = {}
+    tmp = []
+    for el in items_list:
+        tmp.append(el)
+    data['names'] = tmp
+    return render(request, 'main.html', context=data)
+
+def contacts(request):
+    return render(request, 'contacts.html')
+def delivery(request):
+    return render(request, 'delivery.html')
+def login(request):
+    return render(request, 'login.html')
